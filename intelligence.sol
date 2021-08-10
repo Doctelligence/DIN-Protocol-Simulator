e8ac9d5a0ab24180e8349ae4f3df1f282852ad18
Abraham Nash <61595494+abrahamnash@users.noreply.github.com>
2021-08-10 19:36:28 +0100

    Create intelligence.sol

---
+++ intelligence.sol
@@ -0,0 +1,7 @@
+
+pragma solidity ^0.4.4;
+
+contract Intelligence {
+
+	#Federated Learning Protocol which retrieves a transactions smart contract (time-based epochs) in federated learning access to a growing userbase
+  
