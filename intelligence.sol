9a75cf2d3a07619182761ae8e7fb67e449cccdd6
Abraham Nash <61595494+abrahamnash@users.noreply.github.com>
2023-01-10 20:14:38 +0000

    Update intelligence.sol

--- intelligence.sol
+++ intelligence.sol
@@ -3,5 +3,4 @@ pragma solidity ^0.4.4;
 
 contract Intelligence {
 
-	#Federated Learning Protocol which retrieves a transactions smart contract (time-based epochs) in federated learning access to a growing userbase
   
